35b49ce0754a6cbe12a33bb36cfc5dc15ae3e5a1
boazfrider <53977844+boazfrider@users.noreply.github.com>
2022-04-13 20:28:08 +0300

    Add files via upload

---
+++ Employee.cpp
@@ -0,0 +1,45 @@
+#include "Employee.h"
+#include<iostream>
+
+Employee:: Employee(int employee_id, int company_id, int salary , int grade):employee_id(employee_id),company_id(company_id),salary(salary),grade(grade){}
+
+int Employee::getGrade() {
+     return grade;
+};
+
+int Employee::getSalary(){
+    return salary;
+}
+int Employee::getEmployeeID(){
+    return employee_id;
+}
+int Employee::getCompanyID(){
+    return company_id;
+}
+
+void Employee::setGrade(int grade){
+    this->grade = grade;
+}
+void Employee::setSalary(int salary){
+    this->salary = salary;
+}
+void Employee::setCompanyID(int company_id){
+    this->company_id = company_id;
+}
+
+int Employee::compareBySalary(Employee* temp)
+{
+    if(this->salary < temp->getSalary())
+        return -1;
+    if(this->salary == temp->getSalary())
+        return 0;
+    return 1;
+
+}
+int main(){
+    Employee a;
+    a.setSalary(1000);
+    Employee* b = new Employee(2,4,700,34);
+    int result = a.compareBySalary(b);
+    std::cout<<result;
+}
---
+++ Employee.h
@@ -0,0 +1,40 @@
+
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+
+
+class Employee
+{  
+    int employee_id;
+    int company_id;
+    int salary;
+    int grade; // the rank of the employee
+    
+  
+    
+
+public:
+
+    Employee() = default;
+    Employee(int employee_id, int company_id, int salary , int grade);
+    ~Employee()=default;
+
+
+    int getGrade();
+    int getSalary();
+    int getEmployeeID();
+    int getCompanyID();
+    void setGrade(int grade);
+    void setSalary(int salary);
+    void setCompanyID(int company_id);
+
+    /*compare function by salary
+    if salary < temp.salary return -1;
+    if eqaul - return 0;
+    else return 1;
+    */
+    int compareBySalary(Employee* temp);
+
+
+};
+#endif
