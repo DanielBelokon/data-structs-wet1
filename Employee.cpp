6eef9330bf80cc5c990818f831b86747275cb040
Daniel Belokon <daniel.belokon@gmail.com>
2022-04-17 19:32:22 +0300

    add compare, consts

--- Employee.cpp
+++ Employee.cpp
@@ -1,19 +1,38 @@
 #include "Employee.h"
 #include<iostream>
 
-Employee:: Employee(int employee_id, int company_id, int salary , int grade):employee_id(employee_id),company_id(company_id),salary(salary),grade(grade){}
+Employee::Employee(int employee_id, int company_id, int salary, int grade) : employee_id(employee_id), company_id(company_id), salary(salary), grade(grade) {}
 
-int Employee::getGrade() {
-     return grade;
+bool Employee::compareBySalary(Employee *const &temp1, Employee *const &temp2)
+{
+    if (temp1->getSalary() < temp2->getSalary())
+        return true;
+    else if (temp1->getSalary() > temp2->getSalary())
+        return false;
+    else
+        return temp1->getEmployeeID() > temp2->getEmployeeID();
+}
+
+bool Employee::compareByPointer(Employee *const &temp1, Employee *const &temp2)
+{
+    return temp1->getEmployeeID() < temp2->getEmployeeID();
+}
+
+int Employee::getGrade() const
+{
+    return grade;
 };
 
-int Employee::getSalary(){
+int Employee::getSalary() const
+{
     return salary;
 }
-int Employee::getEmployeeID(){
+int Employee::getEmployeeID() const
+{
     return employee_id;
 }
-int Employee::getCompanyID(){
+int Employee::getCompanyID() const
+{
     return company_id;
 }
 
@@ -23,17 +42,16 @@ void Employee::setGrade(int grade){
 void Employee::setSalary(int salary){
     this->salary = salary;
 }
+void Employee::increaseSalary(int salary)
+{
+    this->salary += salary;
+}
 void Employee::setCompanyID(int company_id){
     this->company_id = company_id;
 }
-
-bool Employee::compareBySalary(Employee *temp)
+void Employee::increaseGrade()
 {
-    if (this->salary < temp->getSalary())
-        return true;
-    if (this->salary == temp->getSalary())
-        return this->employee_id > temp->getEmployeeID();
-    return false;
+    this->grade++;
 }
 
 bool Employee::operator<(const Employee &other) const
