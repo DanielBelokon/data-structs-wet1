442ae6c943c31ff7d9a16a779f8898d899c40855
Daniel Belokon <daniel.belokon@gmail.com>
2022-04-17 19:31:58 +0300

    exceptions

---
+++ Exceptions.h
@@ -0,0 +1,38 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+
+#include <exception>
+
+class CompanyNotFoundException : public std::exception
+{
+};
+
+class EmployeeNotFoundException : public std::exception
+{
+};
+
+class CompanyAlreadyExistsException : public std::exception
+{
+};
+
+class EmployeeAlreadyExistsException : public std::exception
+{
+};
+
+class NegativeValueException : public std::exception
+{
+};
+
+class NotEnoughCompaniesException : public std::exception
+{
+};
+
+class AqcuirementFailedException : public std::exception
+{
+};
+
+class InvalidInputException : public std::exception
+{
+};
+
+#endif // EXCEPTIONS_H
